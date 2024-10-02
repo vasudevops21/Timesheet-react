de4160e7a2777ab2dd60a4df7cca957c52e8218b
vasudevops21 <104905752+vasudevops21@users.noreply.github.com>
2024-10-02 18:49:16 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
+  // Other Jest settings
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',  // This tells Jest to use Babel for JavaScript files
+    '^.+\\.(js|jsx)$': 'babel-jest',  // Ensure Babel is handling JavaScript/JSX files
   },
 };
